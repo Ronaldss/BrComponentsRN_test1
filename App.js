d6ab22079b909d1f50a5df93c045e95f52a22b13
unknown <ronaldcontact2019@gmail.com>
2019-12-17 16:55:43 -0300

    Primeiro teste com BrComponentsRN

---
+++ App.js
@@ -0,0 +1,155 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  *
+//  * @format
+//  * @flow
+//  */
+
+// import React from 'react';
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
+
+// import {
+//   Header,
+//   LearnMoreLinks,
+//   Colors,
+//   DebugInstructions,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// const App: () => React$Node = () => {
+//   return (
+//     <>
+//       <StatusBar barStyle="dark-content" />
+//       <SafeAreaView>
+//         <ScrollView
+//           contentInsetAdjustmentBehavior="automatic"
+//           style={styles.scrollView}>
+//           <Header />
+//           {global.HermesInternal == null ? null : (
+//             <View style={styles.engine}>
+//               <Text style={styles.footer}>Engine: Hermes</Text>
+//             </View>
+//           )}
+//           <View style={styles.body}>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Step One</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
+//                 screen and then come back to see your edits.
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>See Your Changes</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <ReloadInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Debug</Text>
+//               <Text style={styles.sectionDescription}>
+//                 <DebugInstructions />
+//               </Text>
+//             </View>
+//             <View style={styles.sectionContainer}>
+//               <Text style={styles.sectionTitle}>Learn More</Text>
+//               <Text style={styles.sectionDescription}>
+//                 Read the docs to discover what to do next:
+//               </Text>
+//             </View>
+//             <LearnMoreLinks />
+//           </View>
+//         </ScrollView>
+//       </SafeAreaView>
+//     </>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+// export default App;
+
+
+//--------------------------------------------------------------------------------------------------------------------
+
+import {BrContent} from 'brcomponentsrn';
+
+export default function Login() {
+  return (
+ 
+    <ImageBackground 
+        source={require('../assets/background.jpg')} 
+        style={{width:'100%',height:'100%'}}>
+ 
+          <Block flex={1} middle bg='rgba(66,44,140,0.9)'>
+            <Block flex={0.5} botton vw={'100%'} >
+              <Text
+                style={{
+                  fontWeight:'bold',
+                  fontSize:45,
+                  color:'#ccc'
+                }}
+              >
+                BRComponentsRN
+              </Text>
+            </Block>
+            <Block flex={0.4} middle vw={'100%'}  >
+              <TextField vw={'90%'} hint='Username' />
+              <TextField vw={'90%'} hint='Password' />
+              <BrButton  vw={'90%'}  title='Enter'/>
+              <Block alRigth vw={'90%'} style={{marginTop:5}} ><Text style={[styles.label,styles.corLink]}>Esqueci a senha</Text></Block>
+            </Block>
+            <Block middle row flex={0.1}>
+                <Text style={styles.label}>Não tem conta?</Text>
+                <Text style={[styles.label,styles.corLink]}> Crie aqui!</Text>
+            </Block>
+          </Block>
+ 
+    </ImageBackground>
+    
+  );
+}
